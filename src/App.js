9fa71d8c85c0b13313d73bd41febba5932702fac
Shreyaan Seth <Shreyaans41@gmail.com>
2022-03-11 00:12:35 +0530

    bruh

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { usePapaParse } from "react-papaparse";
 
 function App() {
+  const [count, setCount] = React.useState(0);
+  const [parsedCsvData, setParsedCsvData] = useState([]);
+  const { readRemoteFile } = usePapaParse();
+  const url =
+    "https://raw.githubusercontent.com/Shreyaan/vocab-telegram-bot/master/words.csv";
+
+
+  useEffect(
+    function () {
+      readRemoteFile(url, {
+        header: true,
+        worker: true,
+        complete: (results) => {
+          setParsedCsvData(results);
+        },
+      });
+    },
+    [count]
+  );
+let randomNumber = 0
+  useEffect(
+    function () {
+    let  min = Math.ceil(0);
+     let max = Math.floor(5348);
+     randomNumber= Math.floor(Math.random() * (max - min) + min);
+
+     },
+    [count]
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+        readRemoteFile
+      </button>
+      {parsedCsvData.data[randomNumber].word}
     </div>
   );
 }
